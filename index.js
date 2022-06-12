2577a37a5f28d5b34e384d4d0cb36655db93d532
Паша <72346436+Pashabn@users.noreply.github.com>
2022-06-12 23:33:51 +0300

    feat: add index.js file

---
+++ index.js
@@ -0,0 +1,21 @@
+const burger = document.querySelector('.burg')
+const navBar = document.querySelector('nav')
+const li = navBar.querySelectorAll('li')
+
+window.addEventListener('resize', () => {
+    if (navBar.classList.contains('active')) toggleMenu()
+})
+
+burger.onclick = toggleMenu
+li.forEach(i => i.onclick = toggleMenu)
+
+function toggleMenu() {
+    navBar.classList.toggle('active')
+    burger.classList.toggle('active')
+}
+
+
+
+const disemvowel = str => str.replace(/[aeiou]/gi, '')
+
+
